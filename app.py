c9a1669092c7af62d4c40d6769eb9e4a94d4a36c
mpanderson <michael-anderson@ouhsc.edu>
2024-06-18 20:33:19 +0000

    Working version of the game
    Have the game working to the point of specifying doors and playing it.

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
-from flask import Flask, render_template, request
+# app.py
+from flask import Flask, render_template, request, jsonify
 import random
-from random import randint
-from random import seed
-
-
-
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def monty_hall():
-    if request.method == 'POST':
-        #num_doors = int(request.form['num_doors'])
-        #num_simulations = int(request.form['num_simulations'])
-        doors = int(request.form['doors'])
-
-        win_door = randint(1,doors)
-         
-
-        # Perform Monty Hall simulations and calculate statistics
-        # You can implement this logic here
-
-        return render_template('index.html', doors=doors, win_door=win_door)
-
+@app.route('/')
+def index():
     return render_template('index.html')
 
+@app.route('/generate_doors', methods=['POST'])
+def generate_doors():
+    num_doors = int(request.form['num_doors'])
+    winner = random.randint(0, num_doors - 1)
+    return jsonify({'num_doors': num_doors, 'winner': winner})
 
+@app.route('/reveal_goat', methods=['POST'])
+def reveal_goat():
+    data = request.get_json()
+    num_doors = data['num_doors']
+    chosen_door = data['chosen_door']
+    winner = data['winner']
 
+    if chosen_door == winner:
+        goat_doors = [i for i in range(num_doors) if i != winner]
+        revealed_goats = random.sample(goat_doors, num_doors - 2)
+    else:
+        goat_doors = [i for i in range(num_doors) if i != winner and i != chosen_door]
+        revealed_goats = goat_doors
 
-if __name__=='__main__':
-  app.run('0.0.0.0',debug=True)
+    return jsonify({'revealed_goats': revealed_goats})
+
+if __name__ == '__main__':
+    app.run(debug=True)
